99e81f45c3d551069a2f26d303e2e09ace0f246f
dollee <dolleebhatia@users.noreply.github.com>
2018-09-06 17:53:47 -0400

    firstcommit

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,36 +1,106 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
-import lang from '../lang'
+import Brick from '../prefab/Brick'
+import Paddle from '../prefab/Paddle'
+import Ball from '../prefab/Ball'
+
 
 export default class extends Phaser.State {
+
+  constructor(){
+    super()
+    this.ballOnPaddle = true
+  }
+
+
   init() { }
+
   preload() { }
 
   create() {
-    const bannerText = lang.text('welcome')
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#77BFA3',
-      smoothed: false
-    })
-
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
+    console.log(this.game.global)
+    this.setUpText()
+    this.setUpBricks()
+    this.setUpPaddle()
+    this.setUpBall()
   }
 
-  render() {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+  setUpBall(){
+  this.ball = new Ball(this.game)
+  this.game.add.existing(this.ball)
+  this.putBallOnPaddle()
+  }
+
+  putBallOnPaddle(){
+      this.ballOnPaddle = true
+     this.ball.reset(this.paddle.body.x, this.paddle.y - this.paddle.body.height)
+  }
+
+  setUpPaddle(){
+    this.paddle = new Paddle(
+      this.game,
+      this.game.world.centerX,
+      this.game.world.height-200
+    )
+    this.game.add.existing(this.paddle)
+  }
+
+  setUpBricks(){
+    this.bricks = this.game.add.group()
+    this.generateBricks(this.bricks)
+  }
+
+  generateBricks(bricksGroup){
+    let rows = 5
+    let columns = 15
+    let xOffset = 70
+    let yOffset = 45
+    let brick
+
+    for (let y=0; y<rows; y++){
+      for (let x=0; x<columns; x++){
+        brick = new Brick(
+          this.game,
+          x * xOffset,
+          y * yOffset
+        )
+        bricksGroup.add(brick)
+      }
     }
+    let brickGroupWidth = ((xOffset * columns) -  (xOffset - brick.width))/2
+    bricksGroup.position.setTo(
+        this.game.world.centerX - brickGroupWidth,
+        this.game.world.centerY - 250
+    )
+  }
+
+  setUpText(){
+  this.createText(20,20,'left', 'Score: ' + this.game.global.score)
+  this.createText(20,40,'left', 'Lives: ' + this.game.global.lives)
+  this.createText(20,60,'left', 'Level: ' + this.game.global.level)
+}
+
+  createText(xOffset, yOffset, align, text){
+    return this.game.add.text(
+      xOffset,
+      yOffset,
+      text,
+      {
+        font:'18px Arial',
+        fill: '#000',
+        boundsAlignH: align
+      }
+    ).setTextBounds(0,0, this.game.world.width,0)
+  }
+
+update(){
+  if (this.ballOnPaddle){
+    this.ball.body.x = this.paddle.x - (this.ball.width / 2)
+  }
+}
+
+
+  render() {
+
   }
 }
